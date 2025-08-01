8976fc73e882e6d6d5d2a5cf5db81d62648bceec
EDISON LOW <edisonlow0909@gmail.com>
2025-08-01 16:46:51 +0800

    Update

--- nextjs-new-start/app/components/ProductCard.tsx
+++ nextjs-new-start/app/components/ProductCard.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import AddToCart from './AddToCart'
+import styles from './ProductCard.module.css'
+
+
 
 const ProductCard = () => {
   return (
-    <>
+    <div>
       <h2>Product Title</h2>
       <p>Product Description</p>
       <span>Price: $XX.XX</span>
       <AddToCart />
-    </>
+    </div>
   )
 }
 
