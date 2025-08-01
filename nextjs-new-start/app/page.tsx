88b0b8175cc92a45873d269375746c383f2e455e
EDISON LOW <edisonlow0909@gmail.com>
2025-08-01 09:28:53 +0800

    Update

--- nextjs-new-start/app/page.tsx
+++ nextjs-new-start/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import supabase from "@/supabase/supabaseClient";
 
 export default function Home() {
+  console.log(supabase)
   return (
     <main>
       <h1>Hello World</h1>
--- nextjs-new-start/app/users/page.tsx
+++ nextjs-new-start/app/users/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
+  email: string;
 }
 
-export default UsersPage
+const UsersPage = async () => {
+    // Fetch users from an API or database
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {cache: "no-store"});
+  const users: User[] = await response.json();
+  console.log(users);
+
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
